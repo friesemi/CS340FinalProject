7f2447820b6d9e8fd5098cc82e46aec9925494d8
friesen04 <40907762+friesen04@users.noreply.github.com>
2020-11-19 13:18:16 -0800

    restructured app

--- server.js
+++ server.js
@@ -6,72 +6,80 @@
 var express = require('express');
 var path = require('path');
 var mysql = require('./dbcon.js');
+var bodyParser = require('body-parser');
 var app = express();
 var port = 3008;
+
+var handlebars = require('express-handlebars').create({
+	defaultLayout: 'main',
+	});
+
+app.engine('handlebars', handlebars.engine);
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'handlebars');
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('mysql', mysql);
 app.set('port', port);
 
-app.use('/owners', require('./src/owner/owners.js'));
-//app.use('/', require('./src/index.js'));
+
 app.get('/', (req, res) => {
-	res.sendFile(__dirname + '/src/index.html');
+	res.status(200).render('index');
 })
 
 app.get('/index', (req, res) => {
-	res.sendFile(__dirname+ '/src/index.html');
+	res.status(200).render('index');
 })
 
 app.get('/owners', (req, res) => {
-	res.sendFile(__dirname + '/src/owner/owners.html');
+	res.sendFile(__dirname + '/views/owner/owners.html');
 })
 
 app.get('/dog_meet', (req, res) => {
-	res.sendFile(__dirname+ '/src/meet/dog_meet.html');
+	res.sendFile(__dirname+ '/views/meet/dog_meet.html');
 })
 
 app.get('/walkers', (req, res) => {
-	res.sendFile(__dirname + '/src/walker/walkers.html');
+	res.sendFile(__dirname + '/views/walker/walkers.html');
 })
 
 app.get('/create_owner', (req, res) => {
-	res.sendFile(__dirname + '/src/owner/create_owner.html');
+	res.sendFile(__dirname + '/views/owner/create_owner.html');
 })
 
 app.get('/search_owner', (req, res) => {
-	res.sendFile(__dirname + '/src/owner/search_owner.html');
+	res.sendFile(__dirname + '/views/owner/search_owner.html');
 })
 
 app.get('/update_owner', (req, res) => {
-	res.sendFile(__dirname + '/src/owner/update_owner.html');
+	res.sendFile(__dirname + '/views/owner/update_owner.html');
 })
 
 app.get('/add_dog', (req, res) => {
-	res.sendFile(__dirname + '/src/dog/add_dog.html');
+	res.sendFile(__dirname + '/views/dog/add_dog.html');
 })
 
 app.get('/delete_owner', (req, res) => {
-	res.sendFile(__dirname + '/src/owner/delete_owner.html');
+	res.sendFile(__dirname + '/views/owner/delete_owner.html');
 })
 
 app.get('/create_breeder', (req, res) => {
-	res.sendFile(__dirname + '/src/breeder/create_breeder.html');
+	res.sendFile(__dirname + '/views/breeder/create_breeder.html');
 })
 
 app.get('/delete_breeder', (req, res) => {
-	res.sendFile(__dirname + '/src/breeder/delete_breeder.html');
+	res.sendFile(__dirname + '/views/breeder/delete_breeder.html');
 })
 
 app.get('/list_breeders', (req, res) => {
-	res.sendFile(__dirname + '/src/breeder/list_breeders.html');
+	res.sendFile(__dirname + '/views/breeder/list_breeders.html');
 })
 
 app.get('/search_for_breeder', (req, res) => {
-	res.sendFile(__dirname + '/src/breeder/search_for_breeder.html');
+	res.sendFile(__dirname + '/views/breeder/search_for_breeder.html');
 })
 
 app.get('/update_breeder', (req, res) => {
-	res.sendFile(__dirname + '/src/breeder/update_breeder.html');
+	res.sendFile(__dirname + '/views/breeder/update_breeder.html');
 })
 
 app.get('/style.css', (req, res) => {
@@ -79,63 +87,63 @@ app.get('/style.css', (req, res) => {
 })
 
 app.get('/dogs', (req, res) => {
-	res.sendFile(__dirname + '/src/dog/dogs.html');
+	res.sendFile(__dirname + '/views/dog/dogs.html');
 })
 
 app.get('/create_dog', (req, res) => {
-	res.sendFile(__dirname + '/src/dog/create_dog.html');
+	res.sendFile(__dirname + '/views/dog/create_dog.html');
 })
 
 app.get('/delete_dog', (req, res) => {
-	res.sendFile(__dirname + '/src/dog/delete_dog.html');
+	res.sendFile(__dirname + '/views/dog/delete_dog.html');
 })
 
 app.get('/search_dog', (req, res) => {
-	res.sendFile(__dirname + '/src/dog/search_dog.html');
+	res.sendFile(__dirname + '/views/dog/search_dog.html');
 })
 
 app.get('/update_dog', (req, res) => {
-	res.sendFile(__dirname + '/src/dog/update_dog.html');
+	res.sendFile(__dirname + '/views/dog/update_dog.html');
 })
 
 app.get('/create_meet', (req, res) => {
-	res.sendFile(__dirname + '/src/meet/create_meet.html');
+	res.sendFile(__dirname + '/views/meet/create_meet.html');
 })
 
 app.get('/delete_meet', (req, res) => {
-	res.sendFile(__dirname + '/src/meet/delete_meet.html');
+	res.sendFile(__dirname + '/views/meet/delete_meet.html');
 })
 
 app.get('/search_meet', (req, res) => {
-	res.sendFile(__dirname + '/src/meet/search_meet.html');
+	res.sendFile(__dirname + '/views/meet/search_meet.html');
 })
 
 app.get('/update_meet', (req, res) => {
-	res.sendFile(__dirname + '/src/meet/update_meet.html');
+	res.sendFile(__dirname + '/views/meet/update_meet.html');
 })
 
 app.get('/add_meet', (req, res) => {
-	res.sendFile(__dirname + '/src/meet/add_meet.html');
+	res.sendFile(__dirname + '/views/meet/add_meet.html');
 })
 
 app.get('/create_walker', (req, res) => {
-	res.sendFile(__dirname + '/src/walker/create_walker.html');
+	res.sendFile(__dirname + '/views/walker/create_walker.html');
 })
 
 app.get('/update_walker', (req, res) => {
-	res.sendFile(__dirname + '/src/walker/update_walker.html');
+	res.sendFile(__dirname + '/views/walker/update_walker.html');
 })
 
 app.get('/add_walker', (req, res) => {
-	res.sendFile(__dirname + '/src/walker/add_walker.html');
+	res.sendFile(__dirname + '/views/walker/add_walker.html');
 })
 
 app.get('/delete_walker', (req, res) => {
-	res.sendFile(__dirname + '/src/walker/delete_walker.html');
+	res.sendFile(__dirname + '/views/walker/delete_walker.html');
 })
 
 app.get('/search_walker', (req, res) => {
-	res.sendFile(__dirname + '/src/walker/search_walker.html');
+	res.sendFile(__dirname + '/views/walker/search_walker.html');
 })
 
 app.listen(port, () => {
